d320e0b0e313a18f2a26c298aabf010593f9baa1
behdadhp <behdad.hajipour95@gmail.com>
2022-02-21 14:41:11 +0100

    added notif and schedule

--- result.py
+++ result.py
@@ -1,8 +1,12 @@
 from API import RequestAPI
+from calculation import Departure
+from datetime import datetime as dt
+from plyer import notification
+import schedule
 import time as module_time
 
 req = RequestAPI()
-
+dep = Departure()
 
 api = "https://v5.bvg.transport.rest/stops/900000100003/departures?direction=900000161002&duration=11&when=today+time"
 to_office_time = str(input("When do you want to leave the home? wrtie in this format: hh:mm(am/pm): "))
@@ -13,3 +17,26 @@ new_to_office_time_api = api.replace("time",to_office_time)
 
 api_to_home_output = req.url(new_to_home_time_api )
 api_to_work_output = req.url(new_to_office_time_api)
+
+result = dep.departure_time(
+          api_to_home_output,
+          api_to_work_output
+          )
+
+print (result)
+
+def notif():
+    notification.notify(
+        title ="Departure",
+        message ="Your train is leaving in 30 minutes",
+        timeout = 10
+    )
+
+if dt.today().strftime('%a') == 'Sat' or dt.today().strftime('%a') == 'Sun' :
+    print ('You can sleep today!')
+else:
+    for i in result:
+        schedule.every().day.at(i).do(notif)
+    while True:
+        schedule.run_pending()
+        module_time.sleep(1)
